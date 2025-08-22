80f9ac0e177e1a72c167576c23412da9da410fd1
Stephanie Lawal <stephanieelawal@gmail.com>
2025-08-22 21:18:43 +0100

    Outline backend routes

--- backend/note-app/server.py
+++ backend/note-app/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from decorators import db_connector
 
 
 app = Flask(__name__)
@@ -6,6 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "hello world"
+
+@app.route('/new_note', methods=['POST'])
+def addNote():
+    pass
+
+@app.route('/notes/<id>', methods=['GET', 'POST'])
+def getNote(id):
+    pass
+
+@app.route('/notes', methods=['GET'])
+def getAllNotes():
+    pass
+
+@app.route('/notes/delete/<id>', methods=['DELETE'])
+def deleteNote(id):
+    pass
+
 
 
 if __name__ == "__main__":
