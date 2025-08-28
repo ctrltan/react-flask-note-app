1d609b9876fe87081b578680a2a942df9ee4c060
Stephanie Lawal <stephanieelawal@gmail.com>
2025-08-28 11:14:35 +0100

    Homepage set up

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function HomePage() {
+    const [apiData, setApiData] = useState('');
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get('http://127.0.0.1:8000/');
+            setApiData(response.data.message);
+        }
+
+        getData();
+    }, []);
+
     return (
         <div>
-            <header>Hello</header>
+            <header>{apiData}</header>
         </div>
     );
 }
