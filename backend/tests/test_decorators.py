5b5075ac8694d0225ccb14aa070ee9f9e19d09dc
Stephanie Lawal <stephanieelawal@gmail.com>
2025-09-16 23:13:19 +0100

    Add new env database password key to unit tests

--- backend/tests/test_decorators.py
+++ backend/tests/test_decorators.py
@@ -6,7 +6,7 @@ from note_app.decorators import db_connector
 
 @pytest.fixture
 def db():
-    conn = psycopg2.connect(database="postgres", host="localhost", user="postgres", password=os.getenv('DB_PASSWORD'))
+    conn = psycopg2.connect(database="postgres", host="localhost", user="postgres", password=os.getenv('POSTGRES_PASSWORD'))
     cur = conn.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, title VARCHAR(50), contents VARCHAR(1000));''')
     conn.commit()
